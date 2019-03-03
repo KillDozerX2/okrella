2f44238746bf3328bafe0b3ad90f9bab008f7ce0
KillDozerX2 <himanshupant127@gmail.com>
2019-03-03 15:02:37 -0500

    Set up basic react app boiler plate code.

--- OkRellBackend/frontendmain/src/index.js
+++ OkRellBackend/frontendmain/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// main css file import for the index page 
 import './index.css';
+// importing the App component. which will serve as the main component
 import App from './App';
+// this service worker is something cool.
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
