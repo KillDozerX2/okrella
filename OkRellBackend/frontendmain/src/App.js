73ae0f0c36a92be6f1abcb1fa9f7a798b9c8e799
KillDozerX2 <himanshupant127@gmail.com>
2019-03-03 17:36:29 -0500

    Added 'react-router-dom' to the project and it's package.json file.

--- OkRellBackend/frontendmain/src/App.js
+++ OkRellBackend/frontendmain/src/App.js
@@ -1,7 +1,7 @@
 // This is boiler plate react code
 // These imports are just needed for eract to work
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 // Import your components here
 import Navbar from './Components/Navbar'
 
