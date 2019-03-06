70cf9d0a7fe603a57860e633d9ca76eef4d4fa71
KillDozerX2 <himanshupant127@gmail.com>
2019-03-06 05:07:28 -0500

    Added vue frontend.

---
+++ OkRellBackend/frontendmain/src/main.js
@@ -0,0 +1,12 @@
+//This is the main.js file
+// DO NOT TOUCH THIS
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
